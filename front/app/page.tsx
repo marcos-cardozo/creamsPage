c1083e20e38b4b1214c8ec6211d026da2252deb9
marcos-cardozo <marcosnazarenocardozo@gmail.com>
2026-04-16 20:43:23 -0300

    inicialización del front

---
+++ front/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold underline text-blue-600">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
